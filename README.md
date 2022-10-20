2e6e570d3cbd6589a6a6df08505ffce90516c7a8
Bobby Tat <messaroundacc1@hotmail.com>
2022-10-20 12:15:04 -0700

    Updated Readme. Created validation for phone number and passwords along with visuals to aid in highlighting errors.

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Next Steps for project completion:
     <!-- -create 'log in' link, done -->
     -use JS to make form responsive and active
         <!-- -create selected input variation (ie blue border and box shadow) -->
-        -create valid and invalid class for inputs (ie green and red)
-        -create error class for password/make sure passwords match
+        <!-- -create valid and invalid class for inputs (ie green and red) -->
+        <!-- -create error class for password/make sure passwords match -->
         <!-- -create account button checks form completion -->
             -onclick: all good = submit form message, else = error message + toggle invalid class
--- javascript.js
+++ javascript.js
@@ -1,3 +1,10 @@
+function validatePhoneNumber(input_str) {
+  var re = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+
+  return re.test(input_str);
+}
+
+
 function validateForm() {
     let fnameInput = document.forms.myForm.fname.value;
     let lnameInput = document.forms.myForm.lname.value;
@@ -6,7 +13,18 @@ function validateForm() {
     let passwordInput = document.forms.myForm.password.value;
     let cpasswordInput = document.forms.myForm.cpassword.value;
     if (fnameInput == "" || lnameInput == ""|| emailInput == "" || phoneInput == "" || passwordInput == ""|| cpasswordInput == ""){
-      alert("Form is invalid, must be filled out")
+      alert("Form is invalid, must be filled out");
       return false;
-    }
-  }
+    } else if (passwordInput !== cpasswordInput) {
+      alert("Passwords do not match");
+      document.getElementById('password_input').classList.remove('default')
+      document.getElementById('password_input').classList.add('invalid')
+      document.getElementById('cpassword_input').classList.remove('default')
+      document.getElementById('cpassword_input').classList.add('invalid')
+      return false;
+    } else if (!validatePhoneNumber(phoneInput)) {
+      alert("Phone is invalid");
+      document.getElementById('phone_input').classList.toggle('invalid')
+      return false;
+    } else return;
+}
